fd8e469545c33a014009ab3b57d07f28e0f18aad
NeferkareII <knaus.peter@hotmail.com>
2020-09-24 18:14:51 +0200

    initial commit

---
+++ R/imports.R
@@ -0,0 +1,15 @@
+#' @useDynLib BVSTVP, .registration = TRUE
+#' @importFrom Rcpp sourceCpp
+#'
+#' @importFrom stochvol svsample
+#'
+#' @import coda
+#'
+#' @importFrom stats quantile model.response model.matrix sd median rnorm na.pass
+#'
+#' @importFrom methods formalArgs
+#'
+#' @importFrom utils maintainer
+#'
+#' @importFrom zoo index
+NULL
